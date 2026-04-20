6efb1ef0e6132c7f8f2f966af07950bdb3db3bd2
Lindsay Gaudinier <lindsay@Lindsays-MacBook-Air.local>
2026-04-19 22:31:03 -0700

    All exports exporting

--- src/exportToExcel.tsx
+++ src/exportToExcel.tsx
@@ -31,30 +31,92 @@ const ExportToExcel = ({ workoutType, interval, workoutDetails }: ExcelExportTyp
     ws.getCell('A5').value = `Warm up: ${workoutDetails.warmUp}`;
 
     ws.getCell('A7').value = `Main set: ${workoutDetails.mainSetYardage}`;
-    if (workoutType === 'distance') {
+
+    // DISTANCE || THRESHOLD
+    if (workoutType === 'distance' || workoutType === 'threshold') {
       ws.getCell('B8').value =
         ` ${workoutDetails.mainSetDetails.rounds} x ${workoutDetails.mainSetDetails.maxDistance} on the ${readableTime(workoutDetails.mainSetDetails.intervalTime, false)}`;
       ws.getCell('B9').value = `Pace: ${readableTime((interval), false)} per 100`;
-    }
-    else if (workoutType === 'sprint') {
+      ws.getCell('A11').value = `Warm down: ${workoutDetails.coolDown}`;
 
+    }
+    // SPRINT 
+    else if (workoutType === 'sprint') {
       ws.getCell('A8').value = `${workoutDetails.mainSetDetails.rounds} x `;
       ws.getCell('C8').value =
         ` ${workoutDetails.mainSetDetails.sprintRounds} x ${workoutDetails.mainSetDetails.sprintDistance} on the ${readableTime((interval * workoutDetails.mainSetDetails.sprintDistance / 100), false)}`;
       ws.getCell('C9').value = `${workoutDetails.mainSetDetails.easyDistance} easy`;
       ws.mergeCells('A8:A9');
-
       ws.getCell('A8').alignment = {
         vertical: 'middle', horizontal: 'center'
       };
-
       ws.getCell('A8').border = {
         right: { style: 'thin' }
       };
+      ws.getCell('A11').value = `Warm down: ${workoutDetails.coolDown}`;
 
     }
+    // EASY
+    else if (workoutType === 'easy') {
 
-    ws.getCell('A11').value = `Warm down: ${workoutDetails.coolDown}`;
+      ws.getCell('B8').value = `Kick`;
+      ws.getCell('C8').value =
+        ` ${workoutDetails.mainSetDetails.kickRounds} x ${workoutDetails.mainSetDetails.kickDistance}`;
+
+      ws.getCell('B9').value = `Pull`;
+      ws.getCell('C9').value =
+        ` ${workoutDetails.mainSetDetails.pullRounds} x ${workoutDetails.mainSetDetails.pullDistance}`;
+
+      ws.getCell('B10').value = `Drill`;
+      ws.getCell('C10').value =
+        ` ${workoutDetails.mainSetDetails.drillRounds} x ${workoutDetails.mainSetDetails.drillDistance}`;
+
+      ws.getCell('D11').value = `${(workoutDetails.mainSetDetails.drills.join("\r\n"))}`;
+
+      ws.getCell('C11').alignment = { wrapText: true };
+
+
+      ws.getCell('B12').value = `Breath`;
+      ws.getCell('C12').value =
+        ` ${workoutDetails.mainSetDetails.breathRounds} x ${workoutDetails.mainSetDetails.breathDistance}`;
+
+      ws.getCell('D13').value =
+        ` ${workoutDetails.mainSetDetails.breathWorkPatternText} by 50s`;
+
+
+      ws.getCell('B8').border = {
+        right: { style: 'thin' }
+      };
+      ws.getCell('B9').border = {
+        right: { style: 'thin' }
+      };
+      ws.getCell('B10').border = {
+        right: { style: 'thin' }
+      };
+      ws.getCell('B11').border = {
+        right: { style: 'thin' }
+      };
+      ws.getCell('B12').border = {
+        right: { style: 'thin' }
+      };
+
+      ws.getCell('A14').value = `Warm down: ${workoutDetails.coolDown}`;
+
+
+      // kickDistance: kickDistance,
+      // kickRounds: kickRounds,
+      // pullDistance: pullDistance,
+      // pullRounds: pullRounds,
+      // drillDistance: drillDistance,
+      // drillRounds: drillRounds,
+      // drills: drills,
+      // breathDistance: breathDistance,
+      // breathRounds: breathRounds,
+      // breathWorkoutPattern: breathWorkoutPattern,
+      // breathWorkPatternText: breathWorkPatternText,
+      // errorMessage: errorMessage
+
+    }
 
     // STYLING
 
